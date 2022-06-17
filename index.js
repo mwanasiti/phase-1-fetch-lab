1e2568387ca3aae65aa86c2caa0c728d47da8ca8
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-17 11:37:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,58 +1,8 @@
+const url="https://anapioficeandfire.com/api/books"
+
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
-  .then(resp=>resp.json())
-  .then(data=>bookName(data))
-  
+  fetch(url).then(resp => resp.json()).then(data => renderBooks(data))
 }
-function bookName(books){
-  books.forEach(book => {
-    let li=document.createElement("li")
-    let sp=document.createElement("span").innerHTML=book.numberOfPages
-    
-    // li.innerHTML=book.authors
-    li.append(sp)
-    document.getElementById("list").appendChild(li)
-    console.log(book.numberOfPages)
-    
-
-
-  });
-  console.log(books)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function renderBooks(books) {
   const main = document.querySelector('main');
